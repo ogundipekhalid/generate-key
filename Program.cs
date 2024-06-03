f81332581929e8f3f856750a301aff9984d37527
ogundipekhalid <115322801+ogundipekhalid@users.noreply.github.com>
2024-06-03 12:20:59 +0100

    comitt

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Globalization;
+using System.Threading.Tasks.Dataflow;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World! 134dec84-21c2-491b-b5f6-5348e110ff65 ");
+     
+        // Guid guid = Guid.NewGuid();
+            Guid guid = Guid.NewGuid();
+            Console.WriteLine(guid);       
+
+    }
+}
